154863df2fbb9a55675e8296664cb2a94999bbbb
jdluther2020 <jdluther2020+github@gmail.com>
2024-07-17 10:55:44 -0500

    Ongoing updates

--- Lab3/lab_3_main_function.py
+++ Lab3/lab_3_main_function.py
@@ -13,7 +13,7 @@ def translate_text():
         SourceLanguageCode='en',
         TargetLanguageCode='fr'
     )
-    # print(response) # this code is inside the function and will print the contents of the variable 'response'
+    print(response) # this code is inside the function and will print the contents of the variable 'response'
     print(json.dumps(response, ensure_ascii=False, indent=4)) # Print formatted output
 
 def main():
